a3c3ccfd9c05382d35fdf9d7d2b30d37473a73cf
Pat Booth <patrickbooth582@gmail.com>
2019-04-08 18:03:07 +1000

    Print App header

---
+++ program.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    print_header()
+
+def print_header():
+    print('---------------------------------------')
+    print('    Real Estate data analysis App')
+    print('---------------------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
