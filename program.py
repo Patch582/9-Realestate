3e18dcf1d091d52e0d1686e8822a1ae6771e2324
Pat Booth <patrickbooth582@gmail.com>
2019-04-19 13:49:57 +1000

    Addded basic data mining features using Lambda expression

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import statistics
 
 
 def main():
@@ -43,11 +44,36 @@ def load_file(file_name):
 
 def query_data(data):
 
+    data.sort(key= lambda p: p.Sale_price)
+
     # TODO: most expensive house
+    high_purchase = data[-1]    # LAST item in the list
+    print('The most expensive house price is ${:,} with {} beds and {} baths'.format(
+        high_purchase.Sale_price, high_purchase.Beds, high_purchase.Bathrooms
+    ))
+
     # TODO: Least expensive house
+    low_purchase = data[0]     # FIRST item in the list
+    print('The least expensive house price is ${:,} with {} beds and {} baths'.format(
+        low_purchase.Sale_price, low_purchase.Beds, low_purchase.Bathrooms
+    ))
+
     # TODO: Average house price
+    prices = []
+    for pur in data:
+        prices.append(pur.Sale_price)
+
+    ave_price = statistics.mean(prices)
+    print('The average price of homes is {:,}'.format(int(ave_price)))
+
     # TODO: Average house price of 2 bed houses
-    pass
+    prices = []
+    for pur in data:
+        if pur.Beds == 2:
+            prices.append(pur.Sale_price)
+
+    ave_price = statistics.mean(prices)
+    print('The average price of homes withe 2 beds is {:,}'.format(int(ave_price)))
 
 
 if __name__ == '__main__':
